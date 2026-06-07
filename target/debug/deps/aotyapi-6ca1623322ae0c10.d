795fe77f1b02a5ea069322962e2779e9de60b287
bitsixfour <tugirasles@gmail.com>
2026-06-06 19:28:50 -0500

    init

---
+++ target/debug/deps/aotyapi-6ca1623322ae0c10.d
@@ -0,0 +1,5 @@
+/home/will/git/aotyapi/target/debug/deps/aotyapi-6ca1623322ae0c10.d: src/main.rs
+
+/home/will/git/aotyapi/target/debug/deps/libaotyapi-6ca1623322ae0c10.rmeta: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/aotyapi-82c357b9c47dd75a.d
@@ -0,0 +1,5 @@
+/home/will/git/aotyapi/target/debug/deps/aotyapi-82c357b9c47dd75a.d: src/main.rs
+
+/home/will/git/aotyapi/target/debug/deps/libaotyapi-82c357b9c47dd75a.rmeta: src/main.rs
+
+src/main.rs:
